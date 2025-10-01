f67e1825704183e24bbccae821a5dba454dc3478
Yuchen Peng <pengpyc@gmail.com>
2025-09-30 23:09:15 -0400

    add atr to detect drawdown threshold

--- src/chinese_index_etf_strategy.py
+++ src/chinese_index_etf_strategy.py
@@ -316,7 +316,22 @@ class etf_strategy:
             ax2.tick_params(axis='y', labelcolor='orange')
             ax.legend()
             plt.show()
-            
+
+    def atr_drawdown_thresholds(self, period: int = 14) -> float:
+        df = self.df.set_index('date').tail(100).sort_index()
+        high_low = df['high'] - df['low']
+        high_close_prev = (df['high'] - df['close'].shift()).abs()
+        low_close_prev = (df['low'] - df['close'].shift()).abs()
+        tr = pd.concat([high_low, high_close_prev, low_close_prev], axis=1).max(axis=1)
+        # atr = tr.rolling(window=period, min_periods=period).mean()  # SMA version
+        atr = tr.ewm(span=period, adjust=False).mean()            # EMA version
+        self.atr_static = atr.iloc[-2]  # latest ATR, not including today's dynamic prices
+        previous_day = self.df[self.df['date']<datetime.now(BEIJING_TZ).strftime('%Y%m%d')]['date'].max()
+        last_closing = self.df[self.df['date']==previous_day]['close'].values[0]
+        print(f"Last closing price: {last_closing}; ATR: {self.atr_static}")
+        p_breakout = float(input('Enter the breakout price:'))
+        print(f"Did the last closing price stay above breakout price - 0.5*ATR? {last_closing > p_breakout - 0.5*self.atr_static}")
+        
     def print_info(self):
         '''
         Print out the information needed
