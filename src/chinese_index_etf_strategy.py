a63b2a41841b257f0ae5a15911e87c1b7cf001a8
Yuchen Peng <pengpyc@gmail.com>
2026-06-22 23:08:58 -0400

    update chinese etf

--- src/chinese_index_etf_strategy.py
+++ src/chinese_index_etf_strategy.py
@@ -454,7 +454,7 @@ class etf_strategy:
             plt.show()
 
     def calculate_atr(self, period: int = 14) -> float:
-        df = self.realtime_df().set_index('date').tail(100).sort_index()
+        df = self.df.set_index('date').tail(100).sort_index()
         high_low = df['high'] - df['low']
         high_close_prev = (df['high'] - df['close'].shift()).abs()
         low_close_prev = (df['low'] - df['close'].shift()).abs()
@@ -466,7 +466,7 @@ class etf_strategy:
         
     def atr_drawdown_thresholds(self, period: int = 14, realtime=False) -> float:
         self.calculate_atr(period)
-        last_closing = self.realtime_df()['close'].iloc[-1]
+        last_closing = self.df['close'].iloc[-1]
         print(f"Last closing price: {last_closing}; ATR: {self.atr_static}")
         p_breakout = float(input('Enter the breakout price:'))
         print(f"Did the last closing price stay above breakout price - 0.5*ATR? {last_closing > p_breakout - 0.5*self.atr_static}")
